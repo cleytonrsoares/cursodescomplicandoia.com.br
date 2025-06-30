f263c6dfd51bf0ff71a1ad85bc4bd6ee978d8301
cleytonrsoares <cleytonrsoares@gmail.com>
2025-06-30 16:24:05 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  plugins: [react()],
+  base: '/cursodescomplicandoia.com.br/'
 })
